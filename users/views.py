36a22dc60d9bc15dbff6ab45f77b95d1af16f1e5
hismkim1 <33340235+hismkim1@users.noreply.github.com>
2023-06-24 16:09:59 +0900

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import User
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+
+
+class UserListAPI(APIView):
+    def get(self, request):
+        queryset = User.objects.all()
+        print(queryset)
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+
+user_list = UserViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
+
